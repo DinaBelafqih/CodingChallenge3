5d6b5f870e9eb2eae7e9b70fcab3b3b805082d02
dinabelafqih <belafqihdina@gmail.com>
2025-02-02 21:41:10 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,12 @@
+//Task 1 : Product Price Management
+  // Declare an array prices using let and initialize it with at least five numerical values
+  let prices = [10, 20, 30, 40, 50];
+
+  // Add a new price to the array
+  prices.push(60);
+
+  // Remove the first price from the array
+  prices.shift();
+
+  // Log the updated price list to the console
+  console.log(prices);
